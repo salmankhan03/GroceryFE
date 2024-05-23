100ac1d79d11910a68b698b4e260ba90408f02ab
Usman Kapadiya <hirenk@intellidt.com>
2024-05-23 14:53:25 +0530

    load more button position change, when parents category select that time all child selected

--- src/components/LeftSideBar/LeftSideBar.js
+++ src/components/LeftSideBar/LeftSideBar.js
@@ -10,25 +10,70 @@ function LeftSideBarComponents({ categoriesData, brandData, availabilityData, se
   const [priceRange, setPriceRange] = useState([0, maximumPrice]);
   const [categorySearchTerm, setCategorySearchTerm] = useState('');
   const [brandSearchTerm, setBrandSearchTerm] = useState('');
+  const [visibleRecords, setVisibleRecords] = useState(5); 
+  const [visibleBrandRecords, setVisibleBrandRecords] = useState(5);
 
 
-  const handleDataChange = (event,) => {
+  const handleDataChange = (event) => {
     const dataType = event.target.dataset.datatype; // Access the 'datatypes' attribute
-    console.log("types ==>", dataType)
+    const categoryId = parseInt(event?.target?.value, 10);
+
     if (dataType === "Category") {
-      const categoryId = parseInt(event?.target?.value, 10);
-      const updatedCategories = event.target.checked
-        ? [...selectedCategories, categoryId]
-        : selectedCategories.filter(id => id !== categoryId);
-      setSelectedCategories(updatedCategories);
+      handleCategoryChange(categoryId, event.target.checked);
     } else {
       const brandId = parseInt(event?.target?.value, 10);
-      console.log(brandId)
       const updatedBrand = event.target.checked
         ? [...selectedBrands, brandId]
         : selectedBrands.filter(id => id !== brandId);
       setSelectedBrands(updatedBrand);
     }
+  };
+  const handleCategoryChange = (categoryId, isChecked) => {
+    let newSelectedCategories = [...selectedCategories];
+
+    const updateChildCategories = (category, isChecked) => {
+      category.children?.forEach(child => {
+        if (isChecked) {
+          if (!newSelectedCategories.includes(child.id)) {
+            newSelectedCategories.push(child.id);
+          }
+        } else {
+          newSelectedCategories = newSelectedCategories.filter(id => id !== child.id);
+        }
+        updateChildCategories(child, isChecked);
+      });
+    };
+
+    if (isChecked) {
+      newSelectedCategories.push(categoryId);
+      const category = findCategoryById(filteredCategories, categoryId);
+      if (category) {
+        updateChildCategories(category, true);
+      }
+    } else {
+      newSelectedCategories = newSelectedCategories.filter(id => id !== categoryId);
+      const category = findCategoryById(filteredCategories, categoryId);
+      if (category) {
+        updateChildCategories(category, false);
+      }
+    }
+
+    setSelectedCategories(newSelectedCategories);
+  };
+
+  const findCategoryById = (categories, id) => {
+    for (const category of categories) {
+      if (category.id === id) {
+        return category;
+      }
+      if (category.children) {
+        const found = findCategoryById(category.children, id);
+        if (found) {
+          return found;
+        }
+      }
+    }
+    return null;
   };
   const filteredCategories = categoriesData?.filter(category =>
     category.name.toLowerCase().includes(categorySearchTerm.toLowerCase())
@@ -48,13 +93,42 @@ function LeftSideBarComponents({ categoriesData, brandData, availabilityData, se
   const handleClearSearch = () => {
     setCategorySearchTerm('');
   };
-  const handleClearBrand  = () => {
+  const handleClearBrand = () => {
     setBrandSearchTerm('');
+  };
+
+  const handleLoadMore = () => {
+    setVisibleRecords((prevVisible) => prevVisible + 5); // Increase visible records by 5
+  };
+
+  
+  const handleViewLess = () => {
+    setVisibleRecords((prevVisible) => Math.max(5, prevVisible - 5)); // Decrease visible records by 5, minimum 5
+  };
+  const handleLoadMoreBrands = () => {
+    setVisibleBrandRecords((prevVisible) => prevVisible + 5); 
+    console.log("visibleBrandRecords",visibleBrandRecords)
+    console.log("filteredBrands?.length",filteredBrands?.length)
+  };
+  const handleViewLessBrands = () => {
+    setVisibleBrandRecords((prevVisible) => Math.max(5, prevVisible - 5)); // Decrease visible records by 5, minimum 5
   };
   return (
     <div >
       <div className="m-3">
-        <h4>Product Categories</h4>
+        <h4 className="d-flex justify-content-between align-items-center">
+          Categories
+          <span className="load-more-span">
+            <div className="load-more-container pt-0 mt-0">
+              {categoriesData?.length > visibleRecords && (
+                <button className="load-more-button btn btn-sm " onClick={handleLoadMore}>View More</button>
+              )}
+               {visibleRecords > 5 && (
+                <button className="load-more-button btn btn-sm" onClick={handleViewLess}>View Less</button>
+              )}
+            </div>
+          </span>
+        </h4>
         <div className='mt-4'>
           <div className='mb-3 position-relative '>
             <InputComponent
@@ -67,30 +141,41 @@ function LeftSideBarComponents({ categoriesData, brandData, availabilityData, se
             />
             {categorySearchTerm && (
               <div className="position-absolute top-0 end-0 translate-middle-y"
-                style={{marginTop:20,marginRight:10}}
+                style={{ marginTop: 20, marginRight: 10 }}
                 onClick={handleClearSearch}>
                 <i className="fas fa-times"></i>
               </div>
             )}
           </div>
-          <div className='mt-3' style={{ height: 250, overflow: 'auto' }}>
+          <div className='mt-3' style={{ maxHeight: 280, overflow: 'auto' }}>
             <ListComponents
               data={filteredCategories}
               selectedData={selectedCategories}
               handleDataChange={handleDataChange}
               datatypes="Category"
+              recordsDisplay={visibleRecords}
             />
           </div>
         </div>
       </div>
-      <div className='mt-5 m-3'>
+      <div className='m-3 mt-2'>
         <h4>Filter by Price</h4>
-        <div className='mt-4'>
+        <div className='mt-4 m-2'>
           <RangeSliderInput min={0} max={maximumPrice} values={priceRange} filteredPrice={filteredPrice} setFilteredPrice={setFilteredPrice} />
         </div>
       </div>
-      <div className="mt-5 m-3">
-        <h4>Brands</h4>
+      <div className="m-3">
+        <h4 className="d-flex justify-content-between align-items-center">Brands
+        <span className="load-more-span">
+            <div className="load-more-container pt-0 mt-0">
+              {filteredBrands?.length >= visibleBrandRecords  ? (
+                <button className="load-more-button btn btn-sm " onClick={handleLoadMoreBrands}>View More</button>
+              ):(
+                <button className="load-more-button btn btn-sm" onClick={handleViewLessBrands}>View Less</button>
+              )}
+            </div>
+          </span>
+        </h4>
         <div className='mt-4'>
           <div className='mb-3 position-relative'>
             <InputComponent
@@ -101,9 +186,9 @@ function LeftSideBarComponents({ categoriesData, brandData, availabilityData, se
               onChange={handleSearchBrand}
               placeholder="Search Brands..."
             />
-             {brandSearchTerm && (
+            {brandSearchTerm && (
               <div className="position-absolute top-0 end-0 translate-middle-y"
-                style={{marginTop:20,marginRight:10}}
+                style={{ marginTop: 20, marginRight: 10 }}
                 onClick={handleClearBrand}>
                 <i className="fas fa-times"></i>
               </div>
@@ -115,6 +200,7 @@ function LeftSideBarComponents({ categoriesData, brandData, availabilityData, se
               selectedData={selectedBrands}
               handleDataChange={handleDataChange}
               datatypes="Brand"
+              recordsDisplay={visibleBrandRecords}
             />
           </div>
         </div>
--- src/components/ListComponents/ListComponents.js
+++ src/components/ListComponents/ListComponents.js
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
 
-const ListComponents = ({ data, selectedData, handleDataChange, datatypes }) => {
+const ListComponents = ({ data, selectedData, handleDataChange, datatypes, recordsDisplay}) => {
   const [expandedItems, setExpandedItems] = useState([]);
-  const [visibleRecords, setVisibleRecords] = useState(5); // Initially show 5 records
 
   const handleToggleExpand = (itemId) => {
     setExpandedItems((prevExpanded) => {
@@ -14,52 +13,46 @@ const ListComponents = ({ data, selectedData, handleDataChange, datatypes }) => 
     });
   };
 
-  const handleLoadMore = () => {
-    setVisibleRecords((prevVisible) => prevVisible + 5); // Increase visible records by 5
-  };
+ 
 
   const renderCategory = (category) => {
     const isExpanded = expandedItems.includes(category.id);
+    const isChecked = selectedData.includes(category.id);
 
     return (
       <div key={category.id}>
-        <div className='d-flex justify-content-between' onClick={() => handleToggleExpand(category.id)}>
+        <div className='d-flex justify-content-between'>
           <label>
             <input
               type="checkbox"
               value={category.id}
-              checked={selectedData.includes(category.id)}
-              data-datatype={datatypes} 
+              checked={isChecked}
+              data-datatype={datatypes}
               onChange={handleDataChange}
             />
             <span className='pl-2'>{category?.name}</span>
           </label>
           {category?.children?.length > 0 && (
-            <span>
+            <span onClick={() => handleToggleExpand(category.id)}>
               <i className={`fa fa-angle-${isExpanded ? 'down' : 'right'}`}></i>
             </span>
           )}
         </div>
         {category?.children && isExpanded && (
           <div className='margin-left'>
-            {category?.children.slice(0, visibleRecords).map((child) => renderCategory(child))}
+            {category?.children.slice(0, recordsDisplay).map((child) => renderCategory(child))}
           </div>
         )}
       </div>
     );
   };
 
-  const initialRender = data?.slice(0, visibleRecords).map((category) => renderCategory(category));
+  // const initialRender = data?.slice(0, visibleRecords).map((category) => renderCategory(category));
+  const initialRender = data?.slice(0, recordsDisplay).map((category) => renderCategory(category));
 
   return (
     <div className="product-category-sidebar">
       {initialRender}
-      <div className="load-more-container">
-        {data?.length > visibleRecords && (
-          <button className="load-more-button" onClick={handleLoadMore}>Load More</button>
-        )}
-      </div>
-
     </div>
   );
 };
