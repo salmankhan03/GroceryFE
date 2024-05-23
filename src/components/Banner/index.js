6a60ffb8c5eecf18b55f1d1350551c5ba3e58431
Usman Kapadiya <hirenk@intellidt.com>
2024-05-23 14:47:49 +0530

    banner height set

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import './index.css'
 
-const Banner = ({images,content}) => {
+const   Banner = ({images,content}) => {
     return (
         <Container fluid className="banner-container">
             <Row>
                 <Col className="text-center position-relative">
-                    <img src={images} alt="Banner Image" className="img-fluid banner-image"  />
+                    <img src={images} alt="Banner Image" className="img-fluid banner-image" style={{maxHeight:350,width:'100%'}} />
                     {content && 
                         <h1 className="position-absolute top-50 start-50 translate-middle bannerTitle text-capitalize">{content}</h1>
                     }
